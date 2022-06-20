7a88e3fe4b041202da2257715eb5684b5ba50b09
makazemafuu <102394629+makazemafuu@users.noreply.github.com>
2022-06-20 13:02:28 +0200

    Updates

---
+++ Iterations/Iterations/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+
+namespace Iterations
+{
+    class Solution
+    {
+        static void Main(string[] args)
+        {
+
+            int N;
+            int solution;
+            bool number = true;
+
+
+
+            while (number)
+            {
+
+
+            }
+
+        }
+    }
+}
